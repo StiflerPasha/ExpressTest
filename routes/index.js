da1f6f1f4d926319dae1fd0103055f3fb5b79285
StiflerPasha <dvoynospavel@gmail.com>
2019-04-19 17:21:25 +0300

    testing

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,14 @@
 var express = require('express');
 var router = express.Router();
+var vd = require('../videodata');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+    res.render('index', {
+        title: 'Home Express',
+        description: "Learning Express",
+        videodata: vd
+    });
 });
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -2,8 +2,11 @@ var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/', function (req, res, next) {
+    res.render('users', {
+        title: "Users View",
+        names: ["Andrey", "Pasha", "John"]
+    });
 });
 
 module.exports = router;
